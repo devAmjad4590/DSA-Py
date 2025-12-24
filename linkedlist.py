a6845bfb5f56743da8ff2d574d04ff63ea0ecf1b
devAmjad4590 <developeramjad4590@gmail.com>
2025-12-24 17:19:32 +0300

    returned the standard pop implementation

--- linkedlist.py
+++ linkedlist.py
@@ -31,17 +31,17 @@ class LinkedList:
     def pop(self):
         if self.length == 0:
             return None
-        temp = self.tail
-        if self.length < 2:
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
             self.head = None
             self.tail = None
-        else:
-            node = self.head
-            for i in range(self.length - 2):
-                node = node.next
-            self.tail = node
-            self.tail.next = None
-        self.length -= 1
         return temp
 
 linkedlist = LinkedList(4)
