6cd6f707f2219ceab40ded2da4d0c5e6db7154c4
devAmjad4590 <developeramjad4590@gmail.com>
2025-12-24 15:35:38 +0300

    Initilised linkedlist and node classes

---
+++ linkedlist.py
@@ -0,0 +1,18 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        length = 1
+
+
+linkedlist = LinkedList(4)
+
+print(linkedlist.head.value)
+
