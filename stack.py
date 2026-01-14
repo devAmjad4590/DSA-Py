9f3730c56bdf26e2cb0a20d2a1e1b21740fc4bc8
devAmjad4590 <developeramjad4590@gmail.com>
2026-01-14 09:10:44 +0300

    Implemented stack using lists

--- stack.py
+++ stack.py
@@ -2,41 +2,55 @@
 # 1. List
 # 2. Linked List
 
-# And in this example we will implement Linked List
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
 
-class Stack:
-    def __init__(self, value):
-        new_node = Node(value) # The head of the linked list acts as the top of the tail for O(1) pop and push
-        self.top = new_node
-        self.height = 1
+# class Stack:
+#     def __init__(self, value):
+#         new_node = Node(value) # The head of the linked list acts as the top of the tail for O(1) pop and push
+#         self.top = new_node
+#         self.height = 1
+#
+#     def print_stack(self):
+#         temp = self.top
+#         while temp:
+#             print(temp.value)
+#             temp = temp.next
+#
+#     def push(self, value):
+#         new_node = Node(value)
+#         if self.height == 0:
+#             self.top= new_node
+#         else:
+#             new_node.next = self.top
+#             self.top = new_node
+#         self.height += 1
+#         return True
+#
+#     def pop(self):
+#         if self.height == 0:
+#             return None
+#         temp = self.top
+#         self.top = temp.next
+#         temp.next = None
+#         self.height -= 1
+#         return temp
 
-    def print_stack(self):
-        temp = self.top
-        while temp:
-            print(temp.value)
-            temp = temp.next
+
+## List Version of Stack
+class Stack:
+    def __init__(self):
+        self.stack_list = []
 
     def push(self, value):
-        new_node = Node(value)
-        if self.height == 0:
-            self.top= new_node
-        else:
-            new_node.next = self.top
-            self.top = new_node
-        self.height += 1
+        self.stack_list.append(value)
         return True
 
     def pop(self):
-        if self.height == 0:
+        if not self.stack_list:
             return None
-        temp = self.top
-        self.top = temp.next
-        temp.next = None
-        self.height -= 1
-        return temp
+        return self.stack_list.pop()
 
 
