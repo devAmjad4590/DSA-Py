8a560cd9c74c26579d8bf99ea48e25ee076e3f07
devAmjad4590 <developeramjad4590@gmail.com>
2026-05-18 15:47:46 +0300

    created set, get methods for HT

--- HashTable.py
+++ HashTable.py
@@ -6,3 +6,27 @@ class HashTable:
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter) * 23) % len(self.data_map)
+
+    def set_item(self,key,value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index] is not None:
+            for i in range(len(self.data_map[index])):
+                if key == self.data_map[index][i][0]:
+                    return self.data_map[index][i][1]
+        return None
+
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i] is not None:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+
+
